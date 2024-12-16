c9430cba9b5569019049c4d4c7799dbe0c90b486
lalitochka <lalitapoluxovich369@gmail.com>
2024-12-16 16:33:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Лалита1";
+  const greeting = "Это тестовый проект Лалита";
 
   console.log('This is App ');
   return (
